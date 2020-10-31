f6a241d720b2bcbdbc5c3937ce3b228a235cec02
mccamatt92 <matthias.mccarthy@gmail.com>
2020-10-31 18:08:52 +0100

    Small Cleanup

--- server/src/index.js
+++ server/src/index.js
@@ -4,41 +4,30 @@ const helmet = require('helmet')
 const cors = require('cors');
 
 
+const middleWares = require('./middlewares')
+
+
 const app = express();
 app.use(morgan('common'));
 app.use(helmet());
 app.use(cors({
-    origin:'http://localhost:3000',
+    origin: 'http://localhost:3000',
 }))
 
 
 
-app.get("/",(req,res)=>{
+app.get("/", (req, res) => {
     res.json({
-        message:'Hello World'
+        message: 'Hello World'
     });
 });
 
-app.use((req,res,next)=>{
-    const error = new Error(`Not Found - ${req.originalUrl}`);
-    res.status(404);
-    next(error);
-});
-
-// eslint-disable-next-line no-unused-vars
-app.use((error, req, res, next)=>{
-    const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
-    res.status(statusCode);
-    res.json({
-        message:error.message,
-        stack:process.env.NODE_ENV === 'production' ? statusCode : error.stack
-    })
-
-})
+app.use(middleWares.notFound);
+app.use(middleWares.errorHandle);
 
 const port = process.env.port || 1337;
 
 
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log(`Listening at port ${port}`)
 })
