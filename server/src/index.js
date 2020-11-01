afb0cf9ae5186bc3b2451cb24a0bce78f6a1f12d
mccamatt92 <matthias.mccarthy@gmail.com>
2020-11-01 19:33:05 +0100

    Added db, api and other requirements

--- server/src/index.js
+++ server/src/index.js
@@ -2,25 +2,33 @@ const express = require('express');
 const morgan = require('morgan')
 const helmet = require('helmet')
 const cors = require('cors');
+const mongoose = require('mongoose');
 
 
-const middleWares = require('./middlewares')
-
+require('dotenv').config();
+const middleWares = require('./middlewares');
+const logs = require('./api/logs');
 
 const app = express();
+
+mongoose.connect(process.env.DATABASE_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
 app.use(morgan('common'));
 app.use(helmet());
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin: process.env.CORS_ORIGN,
 }))
-
-
 
 app.get("/", (req, res) => {
     res.json({
         message: 'Hello World'
     });
 });
+
+app.use('/api/logs',logs);
 
 app.use(middleWares.notFound);
 app.use(middleWares.errorHandle);
